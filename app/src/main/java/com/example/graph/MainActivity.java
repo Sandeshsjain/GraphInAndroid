66f4f7ce52544b1cafb83528a854a58fd8314f55
Sandeshsjain <sandesh.jain.7374@gmail.com>
2020-10-21 11:08:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/graph/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.graph;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    BarChart barChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        barChart = (BarChart) findViewById(R.id.BarChart);
+
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setMaxVisibleValueCount(50);
+        barChart.setPinchZoom(false);
+        barChart.setDrawGridBackground(true);
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+
+        barEntries.add(new BarEntry(1, 40));
+        barEntries.add(new BarEntry(2, 30));
+        barEntries.add(new BarEntry(3, 35));
+        barEntries.add(new BarEntry(4, 45));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Dataset1");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarData data = new BarData(barDataSet);
+        data.setBarWidth(0.9f);
+        barChart.setData(data);
+        XAxis xAxis = barChart.getXAxis();
+    }
+
+}
